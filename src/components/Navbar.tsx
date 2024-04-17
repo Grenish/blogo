a04094b4e94c26984dfe6199ed0437d6a8e422f2
Grenish rai <ariestheracer@gmail.com>
2024-04-17 21:13:02 +0530

    Update Navbar component with new header and navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,18 @@
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav className="flex justify-between items-center p-2 w-[90%] m-auto">
+        <div className="">Blogo</div>
+        <div className="">
+          <ul className="flex gap-4">
+            <li>Home</li>
+            <li>Search</li>
+            <li>Login</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
