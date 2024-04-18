2390a51d4a2bbee3aabb137f069b9f0cd4e6094c
Grenish rai <ariestheracer@gmail.com>
2024-04-18 07:37:35 +0530

    Add custom fonts and colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        germania: ["Germania One", "system-ui"],
+        play: ["Playfair Display", "serif"],
+      },
+      colors: {
+        mun: "#2292A4",
+        cir: "#BDBF09",
+        coa: "#D96C06",
+
+        mel: "#F4B9B2",
+        tea: "#DAEDBD",
+        sky: "#7DBBC3",
+      },
+    },
   },
   plugins: [],
 };
