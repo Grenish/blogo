125437201b4314e86ffe361372c5d2ab9148d8e3
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-01-04 23:11:28 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Blogo",
+  description: "News, reviews, and guides for the modern developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/pages/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
